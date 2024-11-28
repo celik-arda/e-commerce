a8bd217379dd93d0850b256c4d861f0748bb3b61
celik-arda <ardacelik.dev@gmail.com>
2024-11-28 21:33:03 +0300

    feat(Header.tsx/ & hero_section_img.jpg): load image and complete hero section
    corp image for hero and add it, complete hero section temporarily for only one screen width (appr. 1500px)
    
    Signed-off-by: celik-arda <“ardacelik.dev@gmail.com”>

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,9 @@
 import { NavLink } from 'react-router-dom'
 import style from './Header.module.css';
 
+// importing hero_section_image from public //
+import hero_image from '../../../public/hero_section_img_rendered.jpg';
+
 const Header = () => {
     return (
         <div className={style.header_container}>
@@ -26,8 +29,20 @@ const Header = () => {
                     </nav>
                 </div>
             </div>
-            <div className='header_hero_area'>
-                <div className='hero_section'></div>
+            <div className={style.hero_section}>
+                <div className={style.hero_left_side}>
+                    <h2>Everything you can imagine is here for you to buy</h2>
+                    <p>Everything you need is just a click away. Discover innovative products, exceptional service, and a seamless shopping experience. Explore now and simplify your life</p>
+                    <div className={style.hero_buttons}>
+                        <button type='submit'>Signin</button>
+                        <button type='submit'>Signup</button>
+                    </div>
+                </div>
+                <div className={style.hero_right_side}>
+                    <div className={style.hero_img_container}>
+                        <img className={style.hero_image} src={hero_image} alt='heroImage' />
+                    </div>             
+                </div>
             </div>
         </div>
     )
