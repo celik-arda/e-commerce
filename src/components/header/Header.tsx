88ccd1b03ab1b58c8293dbaf912ad6a27e259964
celik-arda <ardacelik.dev@gmail.com>
2024-12-09 18:59:22 +0300

    style(Header.tsx): delete unneccessary codes in comment
    remove codes that doesn't use and are in comment line

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,3 @@
-import { NavLink } from 'react-router-dom'
 import style from './Header.module.css';
 
 // importing hero_section_image from public //
@@ -8,27 +7,7 @@ const Header = () => {
     return (
         <div className={style.header_container}>
 
-            <div className={style.header_menu}>
-                <div className={style.site_name}>
-                    <span> O </span>
-                    <h1>E-Commerce</h1>
-                </div>
-                <div className={style.nav_container}>
-                    <nav>
-                        <div className={style.navbar}>
-                            <NavLink to='/'>Home</NavLink>
-                            <NavLink to='/categories'>Categories</NavLink>
-                            <NavLink to='/basket'></NavLink>
-                            <NavLink to='/signin'>
-                                <button className='button_dark' type='submit'>Signin</button>
-                            </NavLink>
-                            <NavLink to='/signup'>
-                                <button className='button_dark' type='submit'>SignUp</button>
-                            </NavLink>
-                        </div>
-                    </nav>
-                </div>
-            </div>
+
             <div className={style.hero_section}>
                 <div className={style.hero_left_side}>
                     <h2>Everything you can imagine is here for you to buy</h2>
