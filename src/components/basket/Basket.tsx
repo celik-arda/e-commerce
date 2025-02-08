50e5219a1e1ce26a28a9aa5c1dd2763313709ab9
celik-arda <ardacelik.dev@gmail.com>
2025-02-08 23:59:26 +0300

    feat (basket/) : complete basketPage as design and mekanism
    simplify react codes, create function to display basket products in screen, design page and prepare navlink for payment page.

--- src/components/basket/Basket.tsx
+++ src/components/basket/Basket.tsx
@@ -1,16 +1,16 @@
-import style from './Basket.module.css'
-import {Navigate} from 'react-router-dom'
+import {Navigate, NavLink} from 'react-router-dom'
 import {useEffect, useState, useContext} from 'react';
 import {onAuthStateChanged} from 'firebase/auth'
 // import {auth} from '../../../firebase.tsx'
 import MyAllContext from '../../contextProviders/MyContextProvider.tsx';
 import { AllProducts } from '../../models/Product.tsx';
-
+import style from './Basket.module.css'
 
 
 const Basket = () => {
 
     const [allBasketItems, setAllBasketItems] = useState<AllProducts[]>([])
+    const [totalBasketPrice, setTotalBasketPrice] = useState<number>(0)
 
     const contextVariables = useContext(MyAllContext);
     if (!contextVariables) {
@@ -19,47 +19,46 @@ const Basket = () => {
 
     const {auth, user, userAuthState, setUserAuthState} = contextVariables;
 
-
-    // decide to pass or not to basket based on user validation //
-    const checkUserAuthState = async() => {
-
-        onAuthStateChanged(auth, (validUser) => {
+    const displayBasketItems = (storageKey:string) => {
+        const existingStorageItems = sessionStorage.getItem(`${storageKey}`);
+        if (existingStorageItems === null) {
+            return;
+        }
+        else if (existingStorageItems) {
+            const parsedProducts: AllProducts[] = JSON.parse(existingStorageItems);
+            const totalBasketPrice = parsedProducts.reduce((total, item) => total + item.price, 0);
             
-            // user can keep passing to Basket for payment // 
-            if(validUser) {
-                setUserAuthState(true);
-                return "success";
-            }
-            else if (!validUser)
-                setUserAuthState(false);
-        })
+            // display all products in basket page //
+            setAllBasketItems(parsedProducts);
+
+            // display total price that user will pay //
+            setTotalBasketPrice(totalBasketPrice);
+        }
     }
-
-
+    
     useEffect(() => {
-        checkUserAuthState();
-        if (!auth || !user) {
-            console.log("sfasafsdaf")
-        }
-        if (auth && user) {
+        // decide to pass or not to basket based on user validation //
+        onAuthStateChanged(auth, (validUser) => {
+            if (!validUser) {
+                setUserAuthState(false);
+            }
+            else if (validUser) {
+                setUserAuthState(true);
+                displayBasketItems("user_basket");
+            }
+        })
 
-            let basketIncludings = sessionStorage.getItem(`basket__${user.uid}`)
-            console.log("session kontrol : ",basketIncludings, typeof basketIncludings);
-            if(basketIncludings === null){
-                sessionStorage.setItem(`basket__${user.uid}`,JSON.stringify([]));
-            }
-            else{
-                const parsedBasketItems: AllProducts[] = JSON.parse(basketIncludings);
-                console.log("oturumda çekilenler : ",parsedBasketItems,typeof parsedBasketItems);
-                setAllBasketItems([...parsedBasketItems]);
-                console.log("güncellenen array : ",allBasketItems)
-            }
-        }
     },[])
 
 
-        // user has logged in, consumer can pass the payment //
-        if (userAuthState){
+    if (!userAuthState) {
+        return (
+            // <Navigate to='/signin' replace />
+            <div><h1>Please login to continue</h1></div>
+        )
+    }
+    // user has logged in, consumer can pass to basket //
+    else if (userAuthState){
 
             return (
 
@@ -75,15 +74,18 @@ const Basket = () => {
                                 (<li>your basket is empty</li>)
                                 :
                                 (allBasketItems.map((item, index) => (
-                                <li key={index} className={style.list_item}>
+                                <li key={index}>
                                     <div className={style.img_box}>
-                                        <img alt='basket_img' />
+                                        <img src={item.thumbnail} alt='basket_img' />
                                     </div>
                                     <div className={style.title_box}>
                                         {item.title}
                                     </div>
                                     <div className={style.price_box}>
-                                    
+                                        {item.price} £
+                                    </div>
+                                    <div className={style.remove_box}>
+                                        <button className={style.remove_button}>remove</button>
                                     </div>
                                 </li>
                             )))
@@ -93,25 +95,23 @@ const Basket = () => {
                         </ul>
                     </div>
                     <div className={style.basket_info_area}>
-                        X piece of product in your basket
-                    </div>
-                    <div className={style.price_area}>
-                        <h3>Total Price</h3>
-                        <div>XYZ.xx Pound</div>
+                        <div className={style.product_piece_area}>
+                            <span>{allBasketItems.length}</span>
+                            <h4>piece of product in your basket</h4>
+                        </div>
+                        <div className={style.price_area}>
+                            <h4>Total Price : </h4>
+                            <span>{totalBasketPrice}</span>
+                            <span>£</span>
+                        </div>
                     </div>
                     <div className={style.button_area}>
-                        <button>Go to next page</button>
+                        <button className={style.continue_button}><NavLink to='/address_payment'>Continue</NavLink></button>
                     </div>
                 
                 </div>
             )
-        }
-        else if (!userAuthState) {
-            return (
-                // <Navigate to='/signin' replace />
-                <div><h1>BASKET COMPONENT DEVAM EDEMEZ</h1></div>
-            )
-        }
+    }
 }
         
 
