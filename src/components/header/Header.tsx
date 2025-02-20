5c593296b3cf52a525423c84e61049951c807e3c
celik-arda <ardacelik.dev@gmail.com>
2025-02-20 22:50:03 +0300

    fix (Header.tsx) : add missing NavLinks
    convert passive buttons to active NavLinks

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import style from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 // importing hero_section_image from public //
 import hero_image from '../../../public/hero_section_img_rendered.jpg';
@@ -13,8 +14,8 @@ const Header = () => {
                     <h2>Everything you can imagine is here for you to buy</h2>
                     <p>Everything you need is just a click away. Discover innovative products, exceptional service, and a seamless shopping experience. Explore now and simplify your life</p>
                     <div className={style.hero_buttons}>
-                        <button type='submit'>Signin</button>
-                        <button type='submit'>Signup</button>
+                        <NavLink to='/signin'><button type='submit'>Signin</button></NavLink>
+                        <NavLink to='/signup'><button type='submit'>Signup</button></NavLink>
                     </div>
                 </div>
                 <div className={style.hero_right_side}>
