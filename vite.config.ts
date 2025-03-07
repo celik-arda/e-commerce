194c156c5359a936592b55b69e1e39772875089a
c-arda <ardacelik.dev@gmail.com>
2025-03-08 00:42:53 +0300

    docs (root directory/) : add missing config files
    complete missing files to run project or using typescript, vite, eslint etc.

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite'
+import dns from 'dns'
+import react from '@vitejs/plugin-react'
+
+dns.setDefaultResultOrder('verbatim')
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // host: '[::1]',
+    host:'127.0.0.1',
+    port: 4444
+  }
+})
