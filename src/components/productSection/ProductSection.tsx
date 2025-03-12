1dd52a2f9b68ed688da4addfc1d553a90c3c856e
c-arda <ardacelik.dev@gmail.com>
2025-03-13 02:58:39 +0300

    refactor (UIprocess & ProductSection) : add new class
    split geting-displaying data process code, create a utils function and connect to static UIprocess' s prototype

--- src/components/productSection/ProductSection.tsx
+++ src/components/productSection/ProductSection.tsx
@@ -1,13 +1,15 @@
 import style from './ProductSection.module.css'
-import { useState, useEffect, useContext } from 'react';
-import { NavLink } from 'react-router-dom'
+import { useEffect, useContext } from 'react';
 import MyAllContext from '../../contextProviders/MyContextProvider.tsx'
-import { collection, getDocs } from 'firebase/firestore';
-import { Auth, User } from 'firebase/auth';
+import { collection } from 'firebase/firestore';
 import { db } from '../../../firebase.tsx';
-import { Product } from '../../models/Product.tsx'
-import EachProductDetail from '../eachProductDetail/EachProductDetail.tsx';
 import SingleProduct from '../singleProduct/SingleProduct.tsx';
+
+// Product class and AllProducts interface  //
+import { Product, AllProducts } from '../../models/Product.tsx'
+
+// Static class to make UI operations  //
+import { UIprocess } from '../../models/UIprocess.tsx';
 
 
 
@@ -16,32 +18,7 @@ const ProductSection = () => {
     // Variable to fetch neccessary data from firestore //
     const productsRef = collection(db, 'products');
 
-    interface MyContextType {
-        auth: Auth,
-        user: User | null | undefined;
-        isLogging: boolean | undefined;
-        loadingState: boolean;
-        setLoadingState: (newValue: boolean) => void;
-        searchBarValue: string;
-        setSearchBarValue: (value: string) => void;
-        searchResultVisible: boolean;
-        setSearchResultVisible: (value: boolean) => void;
-        allProducts: AllProducts[],
-        setAllProducts: (newValue: AllProducts[] | []) => void,
-        listResult: AllProducts[],
-        setListResult: (newValue: AllProducts[]) => void,
-    }
 
-    interface AllProducts {
-        category: string
-        availabilityStatus: string;
-        title: string;
-        id: number;
-        price: number;
-        description: string;
-        images: any[];
-        thumbnail: string;
-    }
     
     const contextVariables = useContext(MyAllContext)
 
@@ -51,46 +28,16 @@ const ProductSection = () => {
     
     const {allProducts, setAllProducts} = contextVariables;
     
-    let getAllProductsFromFirestore = (collectionRef : any) => {
+    let getProductsFromFirestoreAndList = (collectionRef : any) => {
         
-        getDocs(collectionRef)
-        .then(querySnapshot => {
-            
-            // if my database collection is not empty //
-            if (!querySnapshot.empty){
-                
-                let fetchedProducts: AllProducts[] = querySnapshot.docs.map((e) => {
-                    
-                    let eachProduct = e.data() as AllProducts; 
-                    // create inherited Product object //
-                    return new Product(
-                        eachProduct.id,
-                        eachProduct.title,
-                        eachProduct.category,
-                        eachProduct.price,
-                        eachProduct.description,
-                        eachProduct.images,
-                        eachProduct.thumbnail,
-                        eachProduct.availabilityStatus,
-                    )
-                })
-                setAllProducts(fetchedProducts);
-            }
-            else {
-                console.log("collection is empty !!!");
-            }
-        })
-        .catch(err => {
-            const myErr = err.message;
-            console.log("HATA MESAJI : ",myErr)
-        })
 
-        setAllProducts(allProducts);
+        UIprocess.getAllProductsAndList(collectionRef, setAllProducts);
+
         return allProducts;
     }
     
     useEffect(() => {
-        getAllProductsFromFirestore(productsRef);
+        getProductsFromFirestoreAndList(productsRef);
     },[])
         
         
@@ -98,14 +45,14 @@ const ProductSection = () => {
         <div className={style.product_section}>
             <ul className={style.product_list}>
                 {
-                allProducts.map((item: AllProducts | undefined, index) => (
-                    <li key={index} className={style.product_item}>
-                        <SingleProduct item={item} />
-                    </li>
-                ))
-            }
-            </ul>
+                    allProducts.map((item: AllProducts | undefined, index) => (
 
+                        <li key={index} className={style.product_item}>
+                            <SingleProduct item={item} />
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
