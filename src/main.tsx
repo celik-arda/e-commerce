1d14c5d64833d4cc8d297774dc313178501576b6
c-arda <ardacelik.dev@gmail.com>
2025-03-18 03:40:31 +0300

    chore (main.tsx) add code for vscode workspace
    write 3 lines code without any effect to project, adjust only vs code terminal notifications and filter some of these message

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import { MyContextProvider } from './contextProviders/MyContextProvider.tsx'
 import './index.css'
 import App from './App.tsx'
 
+// workspace config for just output msg filter //
+if (process.env.NODE_ENV === 'development') {
+    console.warn = () => {}; // console.warn'ı boş bir fonksiyon yap
+}
+
 createRoot(document.getElementById('root')!).render(
     
     <BrowserRouter>
