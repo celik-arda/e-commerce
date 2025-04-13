ecc6af7b77e2890452cfead6cad735f96019d6f7
c-arda <ardacelik.dev@gmail.com>
2025-04-13 16:03:19 +0300

    feat (App files & mixins) import responsive mixins
    create smartphone screen-responsive to mixinsResponsive.scss files and import to App.scss file, use @include method for smartphone screen between 350px - 500px

--- src/App.tsx
+++ src/App.tsx
@@ -9,46 +9,12 @@ import MyAllContext from './contextProviders/MyContextProvider.tsx';
 
 //  ---  Page Layout Navbar  --- //
 import Navbar from './layouts/navbar/Navbar.tsx'
-//  ---  (END)  Page Layout Navbar  --- //
 
-
-// ---  Components  ---  //
-import HomePage from './components/homePage/HomePage.tsx'
-import UserProfile from './layouts/authButtonsContainer/userProfile/UserProfile.tsx';
-import Basket from './components/basket/Basket.tsx';
-import SignIn from './components/authComponents/signIn/SignIn.tsx'
-import SignUp from './components/authComponents/signUp/SignUp.tsx'
-import SignOut from './components/authComponents/signOut/SignOut.tsx';
-import EachProductLayout from './layouts/eachProductLayout/EachProductLayout.tsx'
-import EachProductDetail from './components/eachProductDetail/EachProductDetail.tsx'
-// ---  (END)  Components  ---  //
-
+// ---  Firebase Objects For Authentication --- //
 import {Auth, User} from 'firebase/auth'
 
 
-// Inheritance-based Object ""Product //
-import { Product } from './models/Product.tsx'
-import { MyContextType, AllProducts } from './models/Product.tsx'
-
-
 function App() {
-    
-        interface MyContextType {
-            auth: Auth,
-            user: User | null | undefined;
-            isLogging: boolean | undefined;
-            loadingState: boolean;
-            setLoadingState: (newValue: boolean) => void;
-            searchBarValue: string;
-            setSearchBarValue: (value: string) => void;
-            searchResultVisible: boolean;
-            setSearchResultVisible: (value: boolean) => void;
-            allProducts: AllProducts[],
-            setAllProducts: (newValue: AllProducts[] | []) => void,
-            listResult: AllProducts[],
-            setListResult: (newValue: AllProducts[]) => void,
-        }
-
     interface AllProducts {
         category: string
         availabilityStatus: string;
@@ -68,7 +34,7 @@ function App() {
     }
 
         
-        const {auth, user, isLogging, loadingState, setLoadingState, searchBarValue, setSearchBarValue, searchResultVisible, setSearchResultVisible, allProducts, setAllProducts, listResult, setListResult} = contextVariables;
+        const {allProducts} = contextVariables;
         
         let all_items: AllProducts[];
 
