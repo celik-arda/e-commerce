35591ae688e997e737b64176ce66dda172393faa
c-arda <ardacelik.dev@gmail.com>
2025-04-27 23:02:46 +0300

    style (eachProductLAyout & searcProduct): clean line
    remove unnecessary imports and lines, correct error message lines

--- src/layouts/eachProductLayout/EachProductLayout.tsx
+++ src/layouts/eachProductLayout/EachProductLayout.tsx
@@ -1,16 +1,9 @@
 import '../../App.css'
-import {useState, useEffect, ReactNode} from 'react'
-import { Outlet, useParams } from 'react-router-dom';
-import EachProductDetail from '../../components/eachProductDetail/EachProductDetail'
+import { Outlet } from 'react-router-dom';
+
 
 
 const EachProductLayout = () => {
-
-    // const [productLink, setProductLink] = useState("");
-
-    
-    
-
 
     return (
         <Outlet/>
