5bc78f59847a332d79423afd16aa811ea053c8ed
c-arda <ardacelik.dev@gmail.com>
2025-04-28 13:50:15 +0300

    build (All configs): optimize configs to run project
    remove old packages and configs like package.json or yarn.lock, clear cache and install dependencies in clean workspace, optimize config codes compatibility while run the project in classical windows-mac or linux-ubuntu in even virtual machine with yarn and npm package managers, both of them

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,28 @@
-
-
 import { defineConfig } from 'vite';
 import dns from 'dns';
 import react from '@vitejs/plugin-react';
+import os from 'os';
 
-// For Linux-WSL2 stability, keep DNS-processing config // 
+// For Linux-WSL2 stability, keep DNS-processing config
 dns.setDefaultResultOrder('verbatim');
+
+const isWindows = os.platform() === 'win32';
 
 export default defineConfig({
   plugins: [react()],
   
   css: {
     modules: {
-      localsConvention: 'camelCase', //make css modules letters camelCase //
+      localsConvention: 'camelCase', // make CSS module class names camelCase
     },
-
   },
 
   server: {
-    // regular configs to run project in the browser  //
     host: '127.0.0.1',
     port: 4444,
     watch: {
-      usePolling: true,
-      interval: 100,
+      usePolling: isWindows, // Enable polling only on Windows
+      interval: isWindows ? 300 : 100, // Slightly slower polling interval on Windows
     },
   },
 });
